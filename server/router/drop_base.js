07d124c1e4a00c68733aa455f8fabb0a879a49f8
Nicolas RANDRIANARIVO <nrandria@e2r13p10.42.fr>
2018-12-18 18:30:15 +0100

    Routes: CREATE/DROP fixed

--- server/router/drop_base.js
+++ server/router/drop_base.js
@@ -3,12 +3,13 @@ var sql = require('../db/requetes');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req, response) => {
     db.connection.query(sql.drop_database, function(err, res) {
         if (err)
             console.log("DROP DATABASE ERROR: ", err.message);
         else
             console.log("DROP DATABSE SUCCESS!");
+        response.json("DROP_BASE FINSIHED")
     });
 });
 
