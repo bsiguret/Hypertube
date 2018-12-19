5db776f05786586e7761ed8f6b77f2532310b4a7
RiiceBall <hugo.lefan.zhang@gmail.com>
2018-12-19 18:09:49 +0100

    sign in pb bcryptjs

--- server/router/signin.js
+++ server/router/signin.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
-let db = require('../db/db').connection_db;
-let sql = require('../db/requetes');
+const db = require('../db/db').connection_db;
+const sql = require('../db/requetes');
 
 router.post('/', (req, res) => {
-    db.query(sql.get_user, [[req.body.email]], (err, success) => {
+    db.query(sql.get_user, [null, null, req.body.email], async (err, success) => {
         if (err) {
             return res.status(401).json({msg: err.code + ': ' + err.sqlMessage})
         }
-        if (success.length && success[0].password == req.body.password)
+        let password = await bcrypt.hash(req.body.password, 10);
+        console.log(password);
+        if (success.length && success[0].password == password)
             res.json({msg: "OK"});
         else
             res.status(401).json({msg: "L'email ou password incorrect"});
