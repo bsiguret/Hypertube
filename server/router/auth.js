7dcb6c0a38c822dba9e026fe5704b083a0148660
RiiceBall <hugo.lefan.zhang@gmail.com>
2018-12-21 17:12:32 +0100

    add passport jwt, passport 42(not finish)

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,24 +1,25 @@
-var db = require('../db/db');
-var sql = require('../db/requetes');
 const express = require('express');
 const router = express.Router();
 const passport = require('../tools/passport');
+const jwt = require('jsonwebtoken');
 
 // Router pour passport 42
 router.get('/42', passport.authenticate('42'));
 router.get('/42/callback',
     passport.authenticate('42', {
-        failureRedirect: 'http://localhost:3001',
-        // failureMessage: "Fail",
-        // successRedirect: 'http://localhost:3001/home',
-        // successFlash: "OK",
+        failureRedirect: 'http://localhost:3001'
     }),
     function(req, res) {
-        console.log(req.user);
-        res.redirect('http://localhost:3001/home');
+        const user = req.session.passport.user;
+        if (user.err) {
+            res.redirect('http://localhost:3001');
+        }
+        else {
+            const payload = {id: user.id, username: user.username, emai: user.email};
+            const token = jwt.sign(payload, process.env.JWT_KEY, {expiresIn: 86400});
+            res.redirect('http://localhost:3001/home');
+        }
     }
 );
-
-
 
 module.exports = router;
