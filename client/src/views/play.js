0d29901a9f31d5e0d5d029de4d91b3dc5b480249
zhudax <568064702l@gmail.com>
2019-01-07 18:40:51 +0100

    test play

---
+++ client/src/views/play.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import '../assets/css/pageError.scss'
+
+class Play extends Component
+{
+
+  render() {
+    return (
+      <div>test play</div>
+    )
+  }
+}
+  
+ export default Play;
