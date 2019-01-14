09c98800cfd16a140e0b0e224800b46cc924e3ac
RiiceBall <hugo.lefan.zhang@gmail.com>
2019-01-14 12:04:44 +0100

    change token cookie expire time

--- server/router/signin.js
+++ server/router/signin.js
@@ -8,15 +8,15 @@ router.post('/', (req, res) => {
     if (req.body.email && req.body.password) {
         userQuery.findOne({email: req.body.email}).then(user => {
             if (!user || !bcrypt.compareSync(req.body.password, user.password))
-                res.status(401).json({msg: "E-mail or password incorrect"});
+                res.status(403).json({msg: "E-mail or password incorrect"});
             else if (user && bcrypt.compareSync(req.body.password, user.password)) {
                 if (!user.isVerified)
-                    res.status(401).json({msg: "Account not verified"});
+                    res.status(403).json({msg: "Account not verified"});
                 else {
                     let data = {id: user.id, username: user.username, email: user.email};
                     const payload = data;
-                    const token = jwt.sign(payload, process.env.JWT_KEY, {expiresIn: 86400});
-                    res.cookie('token', token, { maxAge: 86400, httpOnly: true });
+                    const token = jwt.sign(payload, process.env.JWT_KEY, {expiresIn: 86400 * 1000});
+                    res.cookie('token', token, { maxAge: 86400 * 1000, httpOnly: true });
                     data = Object.assign(data, {lastname: user.lastname, firstname: user.firstname, language: user.language, profile: user.profile});
                     res.json({user: data});
                 }
