c4fd67c116118fd61ceb211cc2abe598df86a840
Bastien SIGURET <bsiguret@e2r8p15.42.fr>
2019-01-27 20:47:03 +0100

    oauth settings

--- client/src/components/accountForm.js
+++ client/src/components/accountForm.js
@@ -50,36 +50,40 @@ class AccountForm extends Component {
 				>
 					<Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} name='username' value={user.username} onChange={this.handleChange.bind(this)}/>
 				</Form.Item>
+				{!this.props.user.oauth &&
 				<Form.Item
 					validateStatus={err.email ? 'error' : 'success'}
 					help={err.email}
 				>
 					<Input prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />} name='email' type="email" value={user.email} onChange={this.handleChange.bind(this)}/>
-				</Form.Item>
+				</Form.Item>}
 				<Form.Item
 					validateStatus={err.firstname ? 'error' : 'success'}
 					help={err.firstname}
 				>
 					<Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} name='firstname' value={user.firstname} onChange={this.handleChange.bind(this)}/>
 				</Form.Item>
+
 				<Form.Item
 					validateStatus={err.lastname ? 'error' : 'success'}
 					help={err.lastname}
 				>
 					<Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} name='lastname' value={user.lastname} onChange={this.handleChange.bind(this)}/>
 				</Form.Item>
+				{!this.props.user.oauth &&
 				<Form.Item
 					validateStatus={err.npassword ? 'error' : 'success'}
 					help={err.npassword}
 				>
 					<Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} name='npassword' type="password" placeholder="New Password" onChange={this.handleChange.bind(this)}/>
-				</Form.Item>
+				</Form.Item>}
+				{!this.props.user.oauth &&
 				<Form.Item
 					validateStatus={err.cpassword ? 'error' : 'success'}
 					help={err.cpassword}
 				>
 					<Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} name='cpassword' type="password" placeholder="Confirm Password" onChange={this.handleChange.bind(this)}/>
-				</Form.Item>
+				</Form.Item>}
 				<Form.Item
 					validateStatus={err.language ? 'error' : 'success'}
 					help={err.language}
@@ -89,12 +93,13 @@ class AccountForm extends Component {
 							<Select.Option value="french">french</Select.Option>
           </Select>
 				</Form.Item>
+				{!this.props.user.oauth &&
 				<Form.Item
 					validateStatus={err.password ? 'error' : 'success'}
 					help={err.password}
 				>
 					<Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} name='password' type="password" placeholder="Password" onChange={this.handleChange.bind(this)}/>
-				</Form.Item>
+				</Form.Item>}
 				<Form.Item>
 					<Button block type="primary" htmlType="submit" className="signFormButton">
 						Submit
